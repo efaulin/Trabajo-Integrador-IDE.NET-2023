9d097287ef452fe7e8fd02051642e3d13696036a
Eugenio Faulin <pandufaulin@gmail.com>
2023-07-19 01:21:03 -0300

    Carga inicial del proyecto al GITHUB

---
+++ Entidad/Habitacion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    public class TipHab_Precio
+    {
+        int _id;
+        DateTime _fecha;
+        float _precio;
+        TipoHabitacion _tipoHabitacion;
+
+        public int id { get { return _id; } }
+        public DateTime fecha { get { return _fecha; } set { _fecha = value; } }
+        public float precio { get { return _precio; } set { _precio = value; } }
+        public TipoHabitacion tipoHabitacion { get { return _tipoHabitacion; } set { _tipoHabitacion = value; } }
+    }
+
+    public class TipoHabitacion
+    {
+        int _id;
+        string _descripcion;
+        List<TipHab_Precio> _lstTipHab_Precio { get; set; }
+
+        public int id { get { return _id; } }
+        public string descripcion { get { return _descripcion.ToString(); } set { _descripcion = value; } }
+        public float precio => _lstTipHab_Precio[_lstTipHab_Precio.Count() - 1].precio;
+    }
+
+    public class Habitacion
+    {
+    }
+}
---
+++ Entidad/Persona.cs
@@ -0,0 +1,57 @@
+﻿namespace Entidad
+{
+    public abstract class Persona
+    {
+        int _id;
+        string _nombre;
+        string _apellido;
+        string _tipoDocumento;
+        string _nroDocumento;
+
+        public int id { get { return _id; } }
+        public string nombre { get { return _nombre; } set { _nombre = value; } }
+        public string apellido { get { return _apellido; } set { _apellido = value; } }
+        public string tipoDocumento { get { return _tipoDocumento; } set { _tipoDocumento = value; } }
+        public string nroDocumento { get { return _nroDocumento; } set { _nroDocumento = value; } }
+
+        public Persona(int _id, string _nom, string _ape, string _tipDoc, string _nroDoc)
+        {
+            this._id = _id;
+            this._nombre = _nom;
+            this._apellido = _ape;
+            this._tipoDocumento = _tipDoc;
+            this._nroDocumento = _nroDoc;
+        }
+    }
+
+    public class Cliente : Persona
+    {
+        public Cliente(int _id, string _nom, string _ape, string _tipDoc, string _nroDoc) : base(_id, _nom, _ape, _tipDoc, _nroDoc)
+        {
+        }
+    }
+
+    public class Recepcionista : Persona
+    {
+        string usuario { get; set; }
+        string contraseña { get; set; }
+
+        public Recepcionista(int _id, string _nom, string _ape, string _tipDoc, string _nroDoc, string _usr, string _cts) : base(_id, _nom, _ape, _tipDoc, _nroDoc)
+        {
+            this.usuario = _usr;
+            this.contraseña = _cts;
+        }
+    }
+
+    public class Gerente : Persona
+    {
+        string usuario { get; set; }
+        string contraseña { get; set; }
+
+        public Gerente(int _id, string _nom, string _ape, string _tipDoc, string _nroDoc, string _usr, string _cts) : base(_id, _nom, _ape, _tipDoc, _nroDoc)
+        {
+            this.usuario = _usr;
+            this.contraseña = _cts;
+        }
+    }
+}
