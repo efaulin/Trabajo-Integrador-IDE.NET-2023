ccd0e6ab5ae421ff16760a23a8b6154fb50a3c71
Eugenio Faulin <pandufaulin@gmail.com>
2023-08-15 20:49:14 -0300

    Implementacion en consola de la CRUD TipoHabitacion

---
+++ Negocio/Ngc_DBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class DBContext
+    {
+        static Datos.DBContext dB = new Datos.DBContext();
+        static public Datos.DBContext dBContext { get {  return dB; } }
+    }
+}
